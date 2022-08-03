20f8c36c8d95af1d3a83aa05b4c11f34267e4c18
MonguDykrai <kisaihou@gmail.com>
2022-08-03 12:02:25 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+function HOC(Component) {
+  return function (props) {
+    return <Component {...props} />;
+  };
+}
+
+function PlainButton({ children, style }) {
+  return <button style={style}>{children}</button>;
+}
+const HOCButton = HOC(PlainButton);
+
+HOCButton.defaultProps = {
+  style: {
+    background: 'rgba(0, 0, 0, 0.2)',
+  },
+  children: '默认文字',
+};
+
+const Components = {
+  HOCButton,
+};
+
+export default function App() {
+  const [ComponentTree, setComponentTree] = useState([]);
+  useEffect(() => {
+    setComponentTree([
+      ...ComponentTree,
+      {
+        name: 'HOCButton',
+        nodeId: 'abc',
+        parent: null,
+        props: Components['HOCButton'].defaultProps,
+        children: [
+          {
+            name: 'HOCButton',
+            nodeId: 'abcd',
+            parent: null,
+            props: Components['HOCButton'].defaultProps,
+            children: [
+              {
+                name: 'HOCButton',
+                nodeId: 'abcd23',
+                parent: null,
+                props: Components['HOCButton'].defaultProps,
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'HOCButton',
+        nodeId: 'abcde',
+        parent: null,
+        props: Components['HOCButton'].defaultProps,
+        children: [],
+      },
+    ]);
+  }, []);
+
+  const Traverse = (tree) => {
+    return tree.map((leaf) => {
+      const Component = Components[leaf.name];
+      return (
+        <Component key={leaf.nodeId} {...leaf.props}>
+          {leaf.children?.length === 0
+            ? leaf.props.children
+            : Traverse(leaf.children)}
+        </Component>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <PlainButton style={{ color: 'green' }}>hello</PlainButton>
+      <HOCButton style={{ color: 'red' }}>world</HOCButton>
+      {Traverse(ComponentTree)}
+    </div>
+  );
+}
